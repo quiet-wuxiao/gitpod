b0c4b6cd38e1278d31b84a8d1e35cdf06ba09379
Simon Emms <simon@gitpod.io>
2021-09-14 14:19:27 +0100

    [installer]: add ws-proxy to config

---
+++ installer/pkg/components/ws-proxy/constants.go
@@ -0,0 +1,10 @@
+package wsproxy
+
+const (
+	Component      = "ws-proxy"
+	HostHeader     = "x-wsproxy-host"
+	HTTPProxyPort  = 8080
+	HTTPSProxyPort = 9090
+	MetricsPort    = 9500
+	ProbePort      = 60088
+)
